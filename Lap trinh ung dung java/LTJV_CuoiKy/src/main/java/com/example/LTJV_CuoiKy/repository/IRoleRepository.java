24c8378988c3680db6722eded86d61a44defa69d
NNPHONG0210 <117985060+mynameisphongg@users.noreply.github.com>
2025-03-24 14:15:33 +0700

    Delete Lap trinh ung dung java/LTJV_CuoiKy directory

--- Lap trinh ung dung java/LTJV_CuoiKy/src/main/java/com/example/LTJV_CuoiKy/repository/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.LTJV_CuoiKy.repository;
-
-import com.example.LTJV_CuoiKy.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long>{
-    Role findRoleById(Long id);
-}
--- Lap trinh ung dung java/LTJV_CuoiKy/src/main/java/com/example/LTJV_CuoiKy/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LTJV_CuoiKy.repository;
-
-import com.example.LTJV_CuoiKy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
